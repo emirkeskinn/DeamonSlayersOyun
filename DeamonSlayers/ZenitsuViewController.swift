7151b58003314e1403c468656dc73fae5848d267
emirkeskinn <emkeskin41@gmail.com>
2022-03-20 18:43:09 +0300

    first commit

---
+++ DeamonSlayers/ZenitsuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ZenitsuViewController.swift
+//  DeamonSlayers
+//
+//  Created by Emir Keskin on 20.03.2022.
+//
+
+import UIKit
+
+class ZenitsuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
